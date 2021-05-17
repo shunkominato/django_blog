471df195ebb10314c423d7a867849ad9f38628bb
shunkominato <shun19871001@gmail.com>
2021-05-17 22:18:58 +0900

    views

--- api/views.py
+++ api/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic import DetailView
 
 from .models import Blog
 
 class BlogListView(ListView):
     template_name = 'blog/blog_list.html'
+    model = Blog
+
+class BlogDetailView(DetailView):
     model = Blog
